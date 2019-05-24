6997d68a1132cbdec5c6363c7555aa7cfd5c073c
ymcueva <yovanicueva@gmail.com>
2019-05-24 18:47:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-    response.end("Hello World! 2018");
+    response.end("Hello World! 2019");
 });
 
 const port = process.env.PORT || 1337;
